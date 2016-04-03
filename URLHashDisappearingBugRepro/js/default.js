d2a2334a3beafe0db3246f45270c5898f46f03d5
Kagami Sascha Rosylight <saschanaz@outlook.com>
2016-04-03 17:22:27 +0900

    initial commit

---
+++ URLHashDisappearingBugRepro/js/default.js
@@ -0,0 +1,6 @@
+﻿// Minimal template without WinJS
+Windows.UI.WebUI.WebUIApplication.addEventListener("activated", ev => {
+    if (ev.detail[0].kind === Windows.ApplicationModel.Activation.ActivationKind.launch) {
+
+    }
+})
